2723a047db821c60693987d4aa05a0c23bd687ea
Craig Reasbeck <craig.reasbeck@webloansprocessing.com>
2012-09-21 09:26:23 +0100

    Interface updated

--- library/Image/Processor/Adapter/Interface.php
+++ library/Image/Processor/Adapter/Interface.php
@@ -27,5 +27,68 @@
  */
 interface Image_Processor_Adapter_Interface
 {
-    //
+    /**
+     * Opens the file with the appropriate image type handler.
+     *
+     * @param string $fileName
+     */
+    public function open($fileName);
+
+    /**
+     * Saves the image to the detination specified with the newName specified.
+     *
+     * @param string $destination
+     * @param string $newName
+     */
+    public function save($destination=null, $newName=null);
+
+    /**
+     * Outputs the image in it's current state to the screen, setting
+     * the correct mime-type as a content-type header.
+     */
+    public function display();
+
+    /**
+     * Resizes the current image to the specified width and height.
+     *
+     * @param int $width
+     * @param int $height
+     */
+    public function resize($width=null, $height=null);
+
+    /**
+     * Rotates the image by the specified angle.
+     *
+     * @param int $angle
+     */
+    public function rotate($angle);
+
+    /**
+     * Crops the image by the specified parameters.
+     *
+     * @param int $top
+     * @param int $left
+     * @param int $right
+     * @param int $bottom
+     */
+    public function crop($top=0, $left=0, $right=0, $bottom=0);
+
+    /**
+     * Sets the watermark image specified at the location specified.
+     *
+     * @param string $watermarkImage
+     * @param int $positionX
+     * @param int $positionY
+     * @param int $watermarkImageOpacity
+     * @param bool $repeat
+     */
+    public function watermark($watermarkImage, $positionX=0, $positionY=0,
+        $watermarkImageOpacity=30, $repeat=false);
+
+    /**
+     * Checks the dependencies for the adapter.
+     *
+     * @throws Exception
+     */
+    public function checkDependencies();
 }
